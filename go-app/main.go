ff6ca31dddff25e280e0bb3b92c9617d14f5d2e0
baremetal245 <aditya24g@protonmail.com>
2026-09-13 13:16:18 +0530

    wip: incremental update to core

--- go-app/main.go
+++ go-app/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hi from inside a docker container!")
+		fmt.Fprintln(w, "Hello from docker container running on Linux Mint!")
 	})
-	fmt.Println("Local Server starting on :8080")
+	fmt.Println("Linux Server starting on :8080")
 	http.ListenAndServe(":8080", nil)
 }
