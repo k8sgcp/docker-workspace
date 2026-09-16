a89d21ecc23480ba1ea12349913626bd2ae42624
baremetal245 <aditya24g@protonmail.com>
2026-09-16 14:04:19 +0530

    feat: source code change for service

--- java-app/Main.java
+++ java-app/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", exchange -> {
-            String response = "Hello from inside a container!";
+            String response = "Hello from java app running inside docker container";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         });
-        System.out.println("Server starting on :8080");
+        System.out.println("Linux Server starting on :8080");
         server.start();
     }
 }
